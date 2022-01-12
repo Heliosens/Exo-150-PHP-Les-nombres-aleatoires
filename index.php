6834ba261e0216e4d09db095c4673be607adf1dd
Sylvie-B <unsolo@hotmail.fr>
2022-01-12 22:51:44 +0100

    random

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
  */
 
 // TODO Votre code ici.
-
-
-
+$aleas = rand(0, 50);
+echo $aleas;
+echo "<br>";
 /**
  * 2. Générez un nombre aléatoire compris entre 50 et getrandmax, affichez le résultat dans un nouveau <p>
  */
 
 // TODO Votre code ici.
-
-
+$aleas = rand(50, getrandmax());
+echo $aleas;
+echo "<br>";
 /**
  * 3. Générez un nombre aléatoire compris entre 0 et 50
  * ==> si le nombre est inférieur ou égal à 25, affichez : vous avez gagné ( dans un nouveau <p> )
@@ -22,8 +23,14 @@
  */
 
 // TODO Votre code ici.
-
-
+$aleas = rand(0, 50);
+if($aleas <= 25){
+    echo "vous avez gagné";
+}
+else {
+    echo "vous avez perdu";
+}
+echo "<br>";
 /**
  * 4. Créez une fonction qui génère un nombre aléatoire compris entre $max et $min ( paramètres ).
  * ==> Si le nombre est compris entre $max et $max -100, regénérez un nouveau nombre avec un appel récursif.
@@ -31,10 +38,15 @@
  */
 
 // TODO Votre code ici.
+function createAlea ($max, $min){
+    $result = rand($max, $min);
 
+    if($result > ($max - 100)){
+        createAlea($max, $min);
+    }
+    else {
+        return $result;
+    }
+}
 
-
-
-
-
-
+echo createAlea(150, 0);
